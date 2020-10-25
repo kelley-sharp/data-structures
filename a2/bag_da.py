3ce9d7c79ec03a6a9371f5ccf7983fa294eea4c6
kelley <kelleylsharp@gmail.com>
2020-10-24 17:00:21 -0700

    implemented remove from bag

--- a2/bag_da.py
+++ a2/bag_da.py
@@ -1,10 +1,10 @@
 # Course: CS261 - Data Structures
-# Student Name:
-# Assignment:
-# Description:
-# Last revised:
+# Student Name: Kelley Sharp
+# Assignment: A2 - Part 2
+# Description: Implement a Bag ADT class
+# Last revised: 10/24/2020
 
-from dynamic_array import *
+from dynamic_array import DynamicArray
 
 
 class Bag:
@@ -40,15 +40,25 @@ class Bag:
 
     def add(self, value: object) -> None:
         """
-        TODO: Write this implementation
+        Adds a given value to the bag
         """
-        pass
+        
+        # use dynamic array method append to add the given value to the bag
+        self.da.append(value)
 
     def remove(self, value: object) -> bool:
         """
-        TODO: Write this implementation
+        Removes a given value from the bag if a match was found and returns True
+        if no such item that matches the given value is found it returns False
         """
-        pass
+
+        # loop through bag, if an item matches the given value, remove it and return True
+        for i in range(self.size()):
+            if self.da.get_at_index(i) == value:
+                self.da.remove_at_index(i)
+                return True
+
+        return False
 
     def count(self, value: object) -> int:
         """
@@ -74,47 +84,47 @@ class Bag:
 # BASIC TESTING
 if __name__ == "__main__":
 
-    print("\n# add example 1")
-    bag = Bag()
-    print(bag)
-    values = [10, 20, 30, 10, 20, 30]
-    for value in values:
-        bag.add(value)
-    print(bag)
+    # print("\n# add example 1")
+    # bag = Bag()
+    # print(bag)
+    # values = [10, 20, 30, 10, 20, 30]
+    # for value in values:
+    #     bag.add(value)
+    # print(bag)
 
-    print("\n# remove example 1")
-    bag = Bag([1, 2, 3, 1, 2, 3, 1, 2, 3])
-    print(bag)
-    print(bag.remove(7), bag)
-    print(bag.remove(3), bag)
-    print(bag.remove(3), bag)
-    print(bag.remove(3), bag)
-    print(bag.remove(3), bag)
+    # print("\n# remove example 1")
+    # bag = Bag([1, 2, 3, 1, 2, 3, 1, 2, 3])
+    # print(bag)
+    # print(bag.remove(7), bag)
+    # print(bag.remove(3), bag)
+    # print(bag.remove(3), bag)
+    # print(bag.remove(3), bag)
+    # print(bag.remove(3), bag)
 
     print("\n# count example 1")
     bag = Bag([1, 2, 3, 1, 2, 2])
     print(bag, bag.count(1), bag.count(2), bag.count(3), bag.count(4))
 
-    print("\n# clear example 1")
-    bag = Bag([1, 2, 3, 1, 2, 3])
-    print(bag)
-    bag.clear()
-    print(bag)
+    # print("\n# clear example 1")
+    # bag = Bag([1, 2, 3, 1, 2, 3])
+    # print(bag)
+    # bag.clear()
+    # print(bag)
 
-    print("\n# equal example 1")
-    bag1 = Bag([10, 20, 30, 40, 50, 60])
-    bag2 = Bag([60, 50, 40, 30, 20, 10])
-    bag3 = Bag([10, 20, 30, 40, 50])
-    bag_empty = Bag()
+    # print("\n# equal example 1")
+    # bag1 = Bag([10, 20, 30, 40, 50, 60])
+    # bag2 = Bag([60, 50, 40, 30, 20, 10])
+    # bag3 = Bag([10, 20, 30, 40, 50])
+    # bag_empty = Bag()
 
-    print(bag1, bag2, bag3, bag_empty, sep="\n")
-    print(bag1.equal(bag2), bag2.equal(bag1))
-    print(bag1.equal(bag3), bag3.equal(bag1))
-    print(bag2.equal(bag3), bag3.equal(bag2))
-    print(bag1.equal(bag_empty), bag_empty.equal(bag1))
-    print(bag_empty.equal(bag_empty))
-    print(bag1, bag2, bag3, bag_empty, sep="\n")
+    # print(bag1, bag2, bag3, bag_empty, sep="\n")
+    # print(bag1.equal(bag2), bag2.equal(bag1))
+    # print(bag1.equal(bag3), bag3.equal(bag1))
+    # print(bag2.equal(bag3), bag3.equal(bag2))
+    # print(bag1.equal(bag_empty), bag_empty.equal(bag1))
+    # print(bag_empty.equal(bag_empty))
+    # print(bag1, bag2, bag3, bag_empty, sep="\n")
 
-    bag1 = Bag([100, 200, 300, 200])
-    bag2 = Bag([100, 200, 30, 100])
-    print(bag1.equal(bag2))
+    # bag1 = Bag([100, 200, 300, 200])
+    # bag2 = Bag([100, 200, 30, 100])
+    # print(bag1.equal(bag2))
